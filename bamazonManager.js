3ab0e9cf5784b51355f8f8d74cdf9bfe8ac6628d
Stephen Chang <stephenwchang@gmail.com>
2019-05-11 13:17:12 -0400

    bamazonManager 3/4 complete

--- bamazonManager.js
+++ bamazonManager.js
@@ -42,9 +42,65 @@ function displayStock() {
   let query = connection.query('SELECT * FROM products', function(err, res) {
     if (err) throw err;
     console.log(query.sql);
+    console.log('----------------------------------------------------')
+    console.log('Displaying Current Stock');
+    console.log('----------------------------------------------------')
     for (i = 0; i < res.length; i++) {
       console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
     };
     managerPrompt();
   });
 }
+
+function displayLowStock() {
+  let query = connection.query('SELECT * FROM products WHERE stock_quantity < 5', function(err, res) {
+    if (err) throw err;
+    console.log(query.sql);
+    console.log('----------------------------------------------------')
+    console.log('Displaying Low Stock Items');
+    console.log('----------------------------------------------------')
+    for (i = 0; i < res.length; i++) {
+      console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+    };
+    managerPrompt();
+  });
+}
+
+function addToInventory() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: 'Enter an item ID to add stock to',
+      name: 'itemID'
+    },
+    {
+      type: 'number',
+      message: 'How many would you like to add?',
+      name: 'itemAmount'
+    }
+  ]).then(function(inquirerResponse) {
+    let query = connection.query(`SELECT * FROM products WHERE item_id = ${inquirerResponse.itemID}`, function(err, res) {
+      let query = connection.query(`UPDATE products SET stock_quantity = ${res[0].stock_quantity + inquirerResponse.itemAmount} WHERE item_id = ${inquirerResponse.itemID}`, function(err, res) {
+        displayStock();
+      });
+    });
+  });
+}
+
+// incomplete function
+// function addNewStock() {
+//   inquirer.prompt([
+//     {
+//       type: 'input',
+//       message: 'Enter an item ID to add stock to',
+//       name: 'itemID'
+//     },
+//     {
+//       type: 'number',
+//       message: 'How many would you like to add?',
+//       name: 'itemAmount'
+//     }
+//   ]).then(function(inquirerResponse) {
+
+//   });
+// }
